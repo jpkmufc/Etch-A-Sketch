4ca8d7d7b6c10a1693cef42b6c964dfe10c04465
James Kelly <jpkmufc@gmail.com>
2022-04-30 23:10:56 -0500

    Add grid elements, color change on hover, and darken by 10%

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,4 +1,5 @@
-const container = document.querySelector('.grid')
+const container = document.querySelector('.container')
+addGrid();
 
 function addGrid(size = 16) {
     size = Math.sqrt(size) % 1 === 0 ? size : 16;
@@ -18,3 +19,51 @@ function addGrid(size = 16) {
     }
 }
 
+function assignColorFromArray (color) {
+    return `rgba(${color.join(',')})`;
+}
+
+function setColorsTenPercentDarker(color) {
+    let newColor = [];
+    color.forEach(hue => {
+        newColor.push(Math.floor(hue * .9));
+    })
+    return assignColorFromArray(newColor);
+}
+
+function generateRandomColor () {
+    var rgb = []
+
+    for(let i = 0; i < 3; i++) {
+        rgb.push(Math.floor(Math.random() * 255))
+    }
+
+    return assignColorFromArray(rgb);
+}
+
+
+function changeElementColor (e) {
+    const targetElement = e.target;
+    let pases;
+    let newColor;
+    if(targetElement.hasAttribute('data-passes')) {
+        passes = parseInt(targetElement.dataset.passes) + 1
+        let colors = targetElement.style.backgroundColor
+            .replace('rgb', '')
+            .replace('(', '')
+            .replace(')', '')
+            .split(',')
+            .map(hue => parseInt(hue));
+        newColor = setColorsTenPercentDarker(colors);
+    }
+    else {
+        passes = 1;
+        newColor = generateRandomColor();
+    }
+    targetElement.dataset.passes = passes;
+    targetElement.style.backgroundColor = newColor;
+}
+const gridElements = document.querySelectorAll('.grid-element');
+gridElements.forEach(item => {
+    item.addEventListener('mouseenter', changeElementColor);
+})
