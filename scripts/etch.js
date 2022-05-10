a8b23e8038ef769231236220b68c70da2cad6c6a
James Kelly <jpkmufc@gmail.com>
2022-05-10 00:30:20 -0500

    Add grid transformations.

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,22 +1,26 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.grid-container')
 addGrid();
 
+
+
 function addGrid(size = 16) {
-    size = Math.sqrt(size) % 1 === 0 ? size : 16;
-    const sideLength = Math.sqrt(size);
     let gridElement;
     let gridRow;
-    for (let i = 0; i < sideLength; i++) {
+    for (let i = 0; i < size; i++) {
         gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
 
-        for (let i = 0; i < sideLength; i++) {
+        for (let i = 0; i < size; i++) {
             gridElement = document.createElement('div');
             gridElement.classList.add('grid-element');
             gridRow.appendChild(gridElement);
         }
         container.appendChild(gridRow);
     }
+    const gridElements = document.querySelectorAll('.grid-element');
+    gridElements.forEach(item => {
+        item.addEventListener('mouseenter', changeElementColor);
+    })
 }
 
 function assignColorFromArray (color) {
@@ -63,7 +67,94 @@ function changeElementColor (e) {
     targetElement.dataset.passes = passes;
     targetElement.style.backgroundColor = newColor;
 }
-const gridElements = document.querySelectorAll('.grid-element');
-gridElements.forEach(item => {
-    item.addEventListener('mouseenter', changeElementColor);
-})
+
+function resetGrid() {
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = ""
+    addGrid();
+}
+
+function flipGridX() {
+    let gridStack = [];
+    const gridRows = document.querySelectorAll('.grid-row');
+    gridRows.forEach(item => {
+        gridStack.push(item);
+    })
+    gridStack.reverse();
+
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = "";
+
+    gridStack.forEach(item => {
+        gridContainer.appendChild(item);
+    });
+    
+}
+
+
+function flipGridY() {
+    let gridStack = [];
+    let gridElements;
+    let gridRow;
+    const gridRows = document.querySelectorAll('.grid-row');
+
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = "";
+
+
+    gridRows.forEach(row => {
+        gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        
+        gridElements = row.querySelectorAll('.grid-element')
+        gridElements.forEach(element => {
+            gridStack.push(element);
+        })
+
+        while (gridStack.length > 0) {
+            gridRow.appendChild(gridStack.pop());
+        }
+        gridContainer.appendChild(gridRow);
+    });
+    
+}
+
+function flipDiag() {
+    const gridRows = document.querySelectorAll('.grid-row');
+    let gridRowsArr = [];
+    
+    gridRows.forEach(row => {
+        gridRowsArr.push(Array.from(row.children));
+    })
+
+    const gridSize = gridRowsArr.length;
+    let newGridRow;
+    let currentRow;
+
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = "";
+
+
+    for (let i = 0; i < gridSize; i++) {
+        newGridRow = document.createElement('div');
+        newGridRow.classList.add('grid-row');
+
+        for (let j = 0; j < gridSize; j++) {
+            newGridRow.appendChild(gridRowsArr[j][i])
+        }
+
+        gridContainer.appendChild(newGridRow);
+    }
+}
+
+const resetButton = document.querySelector('.button-reset')
+resetButton.addEventListener('click', resetGrid);
+
+const flipxButton = document.querySelector('.button-flipx')
+flipxButton.addEventListener('click', flipGridX);
+
+const flipyButton = document.querySelector('.button-flipy')
+flipyButton.addEventListener('click', flipGridY);
+
+const flipUlBr = document.querySelector('.button-flipDiag')
+flipUlBr.addEventListener('click', flipDiag);
